0447225c7a31c3b34a3b0642674bd36240b5d594
Ricz Attila <ricza@kkszki.hu>
2024-10-02 11:00:51 +0200

    snake kezdés

--- Arkadia/main.js
+++ Arkadia/main.js
@@ -1,7 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const charParam = urlParams.get('karakter');
 
-//If a link is clicked with the character parameter, charParam will get the value of the character parameter
 if (charParam === 'pacman') {
     const script = document.createElement('script');
     script.src = 'pacman.js';
