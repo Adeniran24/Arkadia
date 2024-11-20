94e50cf7de034ee03ad191e593ece20ef365c994
Adeniran Ádám Kristóf <90899862+1Kaguya-sama@users.noreply.github.com>
2024-11-20 10:33:09 +0100

    asd

--- Arkadia/logreg.js
+++ Arkadia/logreg.js
@@ -6,8 +6,9 @@ const signUpForm=document.getElementById('signup');
 signUpButton.addEventListener('click',function(){
     signInForm.style.display="none";
     signUpForm.style.display="block";
-})
+});
+
 signInButton.addEventListener('click', function(){
     signInForm.style.display="block";
     signUpForm.style.display="none";
-})
+});
