543984903d1564c2dbda73c65b198bd242ffcd22
László Tamás <laszlot@kkszki.hu>
2024-12-05 19:10:50 +0100

    Update prof.js

--- prof.js
+++ prof.js
@@ -10,7 +10,7 @@ const profil=document.getElementById('Logged');
 
 
 //ide if es elágazás kell, hogy ha be van jelentkezve, akkor a logreg gombot elrejtsük, a profil gombot megjelenítsük, adatbázis adatoktól függően
-logreg.addEventListener('click',function(){
+/*logreg.addEventListener('click',function(){
     logreg.style.display="none";
     profil.style.display="block";
 });
@@ -18,6 +18,25 @@ logreg.addEventListener('click',function(){
 profil.addEventListener('click', function(){
     logreg.style.display="block";
     profil.style.display="none";
+});
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    // AJAX kérés a PHP állapotellenőrzéshez
+    fetch('loggedin.php') // Itt a PHP fájl neve, amelyik a státuszt adja vissza
+        .then(response => response.json())
+        .then(data => {
+            
+            if (data.logged_in) {                    
+                logreg.style.display="none";
+                profil.style.display="block";
+            } else 
+            {                
+                logreg.style.display="block";
+                profil.style.display="none";
+            }
+        })
+        .catch(error => console.error('Hiba a státusz lekérdezésekor:', error));
 });
 
 }
